6961f4747436d043bf597a4500164a95db4e52d5
Jayesh Karli <jayesh.karli@turtlemint.com>
2022-11-28 13:33:43 +0530

    Removed unwanted new lines from Main class and using constants instead of direct values for hostname and port number for configuring elastic client object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,8 @@ import utils.ElasticOperations;
 import utils.PrintUtil;
 
 public class Main {
-
     public static void main(String[] args) {
-//        ElasticOperationsCv operationsCv = new ElasticOperationsCv();
         ElasticOperations elasticOperations = new ElasticOperations();
-
         try {
             PrintUtil.println("Indexing list of documents");
             elasticOperations.bulkUploadCVData();
@@ -15,7 +12,5 @@ public class Main {
             PrintUtil.println("Exception occurred while indexing one document");
             PrintUtil.println(e.getLocalizedMessage());
         }
-
     }
-
 }
--- src/main/java/config/ElasticHighLevelClient.java
+++ src/main/java/config/ElasticHighLevelClient.java
@@ -6,8 +6,17 @@ import org.elasticsearch.client.RestHighLevelClient;
 
 public class ElasticHighLevelClient {
 
-    static RestClient restClient = RestClient.builder(new HttpHost("127.0.0.1", 9200)).build();
+    private static final int PORT_NUMBER = 9200;
+    private static final String HOSTNAME = "127.0.0.1";
+
+    static RestClient restClient = RestClient.builder(new HttpHost(HOSTNAME, PORT_NUMBER)).build();
     private static RestHighLevelClient client = new RestHighLevelClient(restClient);
 
+    /**
+     *
+     * The client object is configured to connect to localhost on 9200 port number
+     *
+     * @return Returns an instance of RestHighLevelClient from org.elasticsearch.client package
+     */
     public static RestHighLevelClient getClient() { return client; }
 }
